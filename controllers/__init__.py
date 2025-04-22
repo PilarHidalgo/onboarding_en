5b0e4a5012b5566a5588e2b77402324ed19fbf09
PilarHidalgo <a20164253@pucp.edu.pe>
2025-04-22 13:39:16 -0500

    delete

--- controllers/__init__.py
+++
@@ -1,5 +0,0 @@
-# This file makes the controllers directory a Python package
-# It can be left empty or used to expose specific classes
-from controllers.fruit_controller import FruitController
-
-__all__ = ['FruitController']
--- models/__init__.py
+++
@@ -1,5 +0,0 @@
-# This file makes the models directory a Python package
-# It can be left empty or used to expose specific classes
-from models.fruit import Fruit
-
-__all__ = ['Fruit']
